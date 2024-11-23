c8b42dba3d5e33249b6cf181a2e6e9bc4061a3cf
SamSkull19 <sifatsamin19@gmail.com>
2024-11-23 12:37:29 +0600

    Week-4 Day-7 Problem-1 Solved

---
+++ Week-4/Day-7/AssemblyMinimums.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int h = (n * (n-1)) / 2;
+
+        vector <ll> v(h);
+        vector <ll> ans;
+
+        for(int i=0; i<h; i++){
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+
+        int l = 0, r = n - 1, x = 0;
+        while(l < n-1){
+            ans.push_back(v[x]);
+            x += r;
+            r--;
+            l++;
+        }
+
+        for(auto i : ans){
+            cout << i << " ";
+        }
+        cout << 1000000000 << endl;
+    }
+    return 0;
+}
