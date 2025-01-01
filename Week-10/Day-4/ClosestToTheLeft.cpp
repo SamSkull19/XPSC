46d16c619248b88a3068380eb7488dc9ac8ff56d
samskull <samskull>
2025-01-01 20:20:43 +0600

    Week-10 Day-4 Problem-2 Solved

---
+++ Week-10/Day-4/ClosestToTheLeft.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector <int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+
+    for(int i=0; i<k; i++){
+        int x, l = 0, r = n-1, mid;
+        cin >> x;
+
+        int ans = -1;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(x >= v[mid]){
+                ans = mid;
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+
+        cout << ans + 1 << endl;
+    }
+    
+    return 0;
+}
